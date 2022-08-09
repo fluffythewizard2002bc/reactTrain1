09d0430d4959dca8965cd15d126aba0eb800215b
Eli <elikampel@yahoo.com>
2022-08-09 21:09:53 +0300

    css module

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -26,7 +26,7 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`{$styles['form-control']}${isValid && styles.invalid}`}>
+      <div className={`${styles['form-control']}  ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
